e0342ad02f28bab619b54f06d0ded677663ebc55
Ahmed-Nadeem-Malik <ahmedmalik200625@gmail.com>
2026-01-05 03:49:59 +0000

    trying out making a manual http request

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -3,12 +3,30 @@ package org.example
 import java.net.Socket
 
 fun main() {
-    val socket = Socket("localhost",8000)
+    val socket = Socket("learnxinyminutes.com", 80)
+
+    val reader = socket.getInputStream().bufferedReader()
+    val writer = socket.getOutputStream().bufferedWriter()
+
+
+    writer.write("GET /kotlin/ HTTP/1.1\r\n")
+    writer.write("Host: learnxinyminutes.com\r\n")
+    writer.write("Connection: close\r\n")
+    writer.write("\r\n")
+    writer.flush()
+
+    reader.forEachLine { println(it) }
+
+    socket.close()
+
+    /*
 
     val writer = socket.getOutputStream().bufferedWriter()
-    writer.write("this is my name")
+    writer.write("hello world")
     writer.newLine() // submits it to the buffer
     writer.flush() // sends buffer to the server
 
     socket.close()
+
+     */
 }
